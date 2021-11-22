2799bba8a3af961d33ef548b4f9bf0ab5b77aee4
BulatKarimov <bulatkarimov96@gmail.com>
2021-11-22 15:33:06 +0300

    [add] host and port env [add] server config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,18 +4,31 @@ import (
 	"log"
 	"net/http"
 
-	"practics/internal/handler"
-
 	"github.com/go-chi/chi/v5"
+	"github.com/ilyakaznacheev/cleanenv"
+
+	"practics/internal/handler"
+	"practics/internal/config"
 )
 
 func main() {
+	server_config := config.Server{}
+	server_config_boot_err := cleanenv.ReadConfig("config.yml", &server_config)
+
+	if server_config_boot_err != nil {
+		log.Fatal(server_config_boot_err)
+	}
+
 	handler := handler.NewHandler()
 	router := chi.NewRouter()
 
 	router.Get("/path", handler.Res)
 
-	log.Print("Starting Server")
-	err := http.ListenAndServe(":8080", router)
-	log.Fatal(err)
+	log.Print("Starting Server on host: " + server_config.Host + " with port " + server_config.Port)
+	listener_err := http.ListenAndServe(":" + server_config.Port, router)
+
+	if listener_err != nil {
+		log.Fatal(listener_err)
+	}
+	log.Fatal(listener_err)
 }
